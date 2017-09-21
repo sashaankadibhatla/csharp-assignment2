7c55c1bc8ee277a6fce6fae7eb1dcac78ed6dedc
sashaankadibhatla <sashaankadibhatla@gmail.com>
2017-09-21 15:32:17 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+
+   
+namespace csharp_assignment2
+{
+     class MyArray
+    {
+        public  static void  Call(){
+            int []array=new int[12];
+            int i;
+            for(i=1;i<=10;i++)
+            {
+                 array[i]=int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("array crosses 10");
+            }
+            
+        }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the class array");
+            MyArray.Call();
+            Console.WriteLine("Enter the number of terms in fibonacci series");
+            string input=Console.ReadLine();
+            int n=int.Parse(input);
+            int i;
+            int[] array=new int[n+1];
+            array[0]=0;
+            array[1]=1;
+            for(i=2;i<=n;i++)
+            {
+                array[i]=array[i-1]+array[i-2];
+            }
+            Console.WriteLine("Fibonacci Series is :");
+            for(i=0;i<=n;i++)
+                Console.Write(" {0} ",array[i]);
+            
+        }
+    }
+    
+}
+
